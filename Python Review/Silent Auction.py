fb7f6066fc3086c2ec32313ae53134f99d12bbf8
ryanzhao2 <ryan.zhao2@gmail.com>
2022-02-07 15:38:04 -0500

    Ryan commit 2022/02/07

--- Python Review/Silent Auction.py
+++ Python Review/Silent Auction.py
@@ -1,10 +1,16 @@
 def calculate_auction():
     highest_bid = 0
+    highest_bid_name = ''
     number_of_bids = int(input("Enter a number of bids greater than 0 and less than 101: "))
     for i in range(number_of_bids):
         bid_name = str(input("Enter your name: "))
-        bid_amount = int(input("Enter amount that is less than 2000: "))
-        if bid_amount >= highest_bid:
-            return bid_name
+        bid_amount = int(input("Enter an amount that is less or equal to 2000: "))
+        while bid_amount > 2000:
+            bid_amount = int(input("Enter an amount that is less or equal to 2000: "))
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            highest_bid_name = bid_name
+    return highest_bid_name
 
-calculate_auction()
+get_bid = calculate_auction()
+print(get_bid)
