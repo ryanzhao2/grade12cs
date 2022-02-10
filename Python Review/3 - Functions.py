a06a68516de093566a1be86b32ab4194b860f8c5
ryanzhao2 <ryan.zhao2@gmail.com>
2022-02-10 16:58:30 -0500

    Ryan commit 2022/02/10

--- Python Review/3 - Functions.py
+++ Python Review/3 - Functions.py
@@ -69,8 +69,26 @@
 #
 # is_multiple(12, 7)
 
-def right_justify(s, n):
-    space = (n - (len(s))) * ' ')
+# def right_justify(s, n):
+#     space = ((n - (len(s))) * ' ')
+#     print(space + s)
+#
+# right_justify('allen', 20)
 
+def b(z):
+ prod = a(z, z)
+ print(z, prod)
+ return prod
 
-right_justify('allen', 20)
+def a(x, y):
+ x = x + 1
+ return x * y
+
+def c(x, y, z):
+ sum = x + y + z
+ pow = b(sum)**2
+ return pow
+
+x = 1
+y = x + 1
+print(c(x, y+3, x+y))
