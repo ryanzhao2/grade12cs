4dfeb30bb19a66d5235c197d9d2246c93ddfdb39
ryanzhao2 <ryan.zhao2@gmail.com>
2022-02-14 22:22:26 -0500

    Ryan commit 2022/02/14

--- Test.py
+++ Test.py
@@ -11,3 +11,14 @@ fruits = 'banana'
 
 if "ba2132131" in fruits:
   print("yes")
+
+def sum13(nums):
+  sum_list = []
+  for i in range(len(nums)):
+    if i == 0 and nums[0] != 13:
+      sum_list.append(nums[i])
+    elif nums[i] != 13 and nums[i-1] != 13:
+      sum_list.append(nums[i])
+  return sum(sum_list)
+
+print(sum13([2,2,2,2,2,13,10,13,10,10]))
