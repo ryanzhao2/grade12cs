3bf694f8c11ee147aaf0f7fe6e09fc4b3142dd18
ryanzhao2 <ryan.zhao2@gmail.com>
2022-03-01 20:38:36 -0500

    Ryan commit 2022/03/01

--- File IO/5 - glob.py
+++ File IO/5 - glob.py
@@ -1,19 +1,18 @@
-import glob
-import csv
-dict = {}
-all_spotify_files = glob.glob('regional*.csv')
-
-for file in all_spotify_files:
-    with open(file) as spotify_file:
-        spotify_file.readline()
-        spotify_file.readline()
-        reader = csv.reader(spotify_file)
-        for line in reader:
-            if line[1] not in dict:
-                str = f'{line[1]} by {line[2]}'
-                dict[str] = {}
-            elif {line[1]} by {line[2]} in dict:
-                str = f'{line[1]} by {line[2]}'
-
-                print(dict[str])
-print(dict)
+# import glob
+# import csv
+# dict = {}
+# all_spotify_files = glob.glob('regional*.csv')
+# for file in all_spotify_files:
+#     with open(file) as spotify_file:
+#         spotify_file.readline()
+#         spotify_file.readline()
+#         reader = csv.reader(spotify_file)
+#         for line in reader:
+#             #print(file[18:28])
+#             str = f'{line[1]} by {line[2]}'
+#             if str not in dict:
+#                 dict[str] = {}
+#                 dict[str][file[18:28]] = line[3]
+#             else:
+#                 dict[str][file[18:28]] = line[3]
+# print(dict)
