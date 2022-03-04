3f923f958ccd459dc1abeee7085b1673dba56ebb
ryanzhao2 <ryan.zhao2@gmail.com>
2022-03-04 15:10:19 -0500

    Ryan commit 2022/03/04

--- File IO/7.0 - Writing.py
+++ File IO/7.0 - Writing.py
@@ -36,49 +36,49 @@
 # generate_people('people.txt', names, occupations)
 
 #Q2
-# import csv
-# def pokemon_dict(filename):
-#     type_list = []
-#     large_list = []
-#     my_list = []
-#     dict = {}
-#     with open(filename, 'r') as pokemon_file:
-#         reader = csv.DictReader(pokemon_file)
-#         for line in reader:
-#             if line['Type 1'] not in type_list:
-#                 type_list.append(line['Type 1'])
-#                 dict[line['Type 1']] = []
-#                 #print(line)
-#             mini_list = []
-#             for char in type_list:
-#
-#                 if line['Type 1'] == char:
-#                     for stat in line:
-#                         if stat != 'Type 1':
-#                             mini_list.append(line[stat])
-#                     if mini_list[2] == '':
-#                         mini_list.pop(2)
-#                     #print(mini_list)
-#             dict[line['Type 1']].append(mini_list)
-#         return dict
-#
-#
-# def display_pokemon(filename, pokemon_file):
-#     with open(filename, 'w', newline='') as new_pokemon_file:
-#         str_data = ''
-#         writer = csv.writer(new_pokemon_file)
-#         for char in pokemon_file:
-#             length = str(len(pokemon_file[char]))
-#             length += '\n'
-#             char += '\n'
-#             new_pokemon_file.write(char)
-#             new_pokemon_file.write(length)
-#             char = char.replace('\n', '')
-#             data = pokemon_file[char]
-#             for char in data:
-#                 writer.writerow(char)
-#
-#
-# pokemon_info = pokemon_dict('Pokemon.csv')
-# #print(pokemon_info)
-# display_pokemon('pokemon_structure.csv', pokemon_info)
+import csv
+def pokemon_dict(filename):
+    type_list = []
+    large_list = []
+    my_list = []
+    dict = {}
+    with open(filename, 'r') as pokemon_file:
+        reader = csv.DictReader(pokemon_file)
+        for line in reader:
+            if line['Type 1'] not in type_list:
+                type_list.append(line['Type 1'])
+                dict[line['Type 1']] = []
+                #print(line)
+            mini_list = []
+            for char in type_list:
+
+                if line['Type 1'] == char:
+                    for stat in line:
+                        if stat != 'Type 1':
+                            mini_list.append(line[stat])
+                    if mini_list[2] == '':
+                        mini_list.pop(2)
+                    #print(mini_list)
+            dict[line['Type 1']].append(mini_list)
+        return dict
+
+
+def display_pokemon(filename, pokemon_file):
+    with open(filename, 'w', newline='') as new_pokemon_file:
+        str_data = ''
+        writer = csv.writer(new_pokemon_file)
+        for char in pokemon_file:
+            length = str(len(pokemon_file[char]))
+            length += '\n'
+            char += '\n'
+            new_pokemon_file.write(char)
+            new_pokemon_file.write(length)
+            char = char.replace('\n', '')
+            data = pokemon_file[char]
+            for char in data:
+                writer.writerow(char)
+
+
+pokemon_info = pokemon_dict('Pokemon.csv')
+#print(pokemon_info)
+display_pokemon('pokemon_structure.csv', pokemon_info)
