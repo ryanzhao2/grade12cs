e33407045977587e50f6417ee5b61c8bab8f6235
ryanzhao2 <ryan.zhao2@gmail.com>
2022-03-09 20:15:07 -0500

    Ryan commit 2022/03/09

---
+++ File IO Assignment/Data Parsing Assignment.py
@@ -0,0 +1,12 @@
+import csv
+import glob
+
+def netflix():
+    pass
+
+def amazon_prime():
+    pass
+
+
+def disney_plus():
+    pass
--- File IO/1 - Reading and Parsing Basics.py
+++ File IO/1 - Reading and Parsing Basics.py
@@ -1,35 +1,23 @@
-# #Q1
-# gr_9 = []
-# gr_10 = []
-# gr_11 = []
-# gr_12 = []
-# with open('allStudents.txt') as student_file:
-#     for line in student_file:
-#         line = line.replace('\n', '')
-#         line = line.split('\t')
-#         elif line[0] == '09':
-#             upper_first = line[2][0]
-#             upper_last = line[1][0]
-#             new_str = upper_first + line[2][1:].lower() + ' ' + upper_first + line[1][1:].lower()
-#             gr_9.append(new_str)
-#         elif line[0] == '10':
-#             upper_first = line[2][0]
-#             upper_last = line[1][0]
-#             new_str = upper_first + line[2][1:].lower() + ' ' + upper_first + line[1][1:].lower()
-#             gr_10.append(new_str)
-#         elif line[0] == '11':
-#             upper_first = line[2][0]
-#             upper_last = line[1][0]
-#             new_str = upper_first + line[2][1:].lower() + ' ' + upper_first + line[1][1:].lower()
-#             gr_11.append(new_str)
-#         elif line[0] == '12':
-#             upper_first = line[2][0]
-#             upper_last = line[1][0]
-#             new_str = upper_first + line[2][1:].lower() + ' ' + upper_first + line[1][1:].lower()
-#             gr_12.append(new_str)
-#
-#
-# print(gr_9)
+#Q1
+gr_9 = []
+gr_10 = []
+gr_11 = []
+gr_12 = []
+with open('allStudents.txt') as student_file:
+    for line in student_file:
+        line = line.strip().split('\t')
+        year = line[0]
+        name = f'{line[2]} {line[1]}'
+        if year == '09':
+            gr_9.append(name)
+        elif year == '10':
+            gr_10.append(name)
+        elif year == '11':
+            gr_11.append(name)
+        elif year == '12':
+            gr_12.append(name)
+
+print(gr_12)
 
 #Q2
 # valid = []
