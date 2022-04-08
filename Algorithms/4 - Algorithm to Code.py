ba44dafce847e9d293984f0a93d2352efb8bdb1c
ryanzhao2 <ryan.zhao2@gmail.com>
2022-04-08 15:16:06 -0400

    Ryan commit 2022/04/08

--- Algorithms/4 - Algorithm to Code.py
+++ Algorithms/4 - Algorithm to Code.py
@@ -1,23 +1,23 @@
-# def bubble_sort(a_list):
-#     index_of_last_unsorted_item = len(a_list)
-#     swapped = True
-#     while swapped:
-#         swapped = False
-#
-#         for i in range(0, index_of_last_unsorted_item - 1):
-#             if a_list[i] > a_list[i+1]:
-#                 a_list[i], a_list[i+1] = a_list[i+1], a_list[i]
-#                 swapped = True
-#         index_of_last_unsorted_item -= 1
-#     print(a_list)
-# a = [5, 4, 6, 2, 4, 6, 7, 8, 1]
-# bubble_sort(a)
+def bubble_sort(a_list):
+    index_of_last_unsorted_item = len(a_list)
+    swapped = True
+    while swapped:
+        swapped = False
+
+        for i in range(0, index_of_last_unsorted_item - 1):
+            if a_list[i] > a_list[i+1]:
+                a_list[i], a_list[i+1] = a_list[i+1], a_list[i]
+                swapped = True
+        index_of_last_unsorted_item -= 1
+    print(a_list)
+a = [5, 4, 6, 2, 4, 6, 7, 8, 1]
+bubble_sort(a)
 
 def selection_sort(a_list):
     for i in range(len(a_list)-1):
         index_min = i
         minimum = a_list[i]
-        for j in range(i, len(a_list)):
+        for j in range(i+1, len(a_list)):
             if a_list[j] < minimum:
                 index_min = j
                 minimum = a_list[j]
@@ -25,7 +25,7 @@ def selection_sort(a_list):
 
     print(a_list)
 a = [300, 500, 600, 100, 450023, 36, 29, 16, 21, 22, 32, 35, 2, 40, 11, 3, 15, 17, 2, 3]
-#selection_sort(a)
+selection_sort(a)
 
 def insertion_sort(a_list):
     index_sorted = 0
@@ -42,5 +42,5 @@ def insertion_sort(a_list):
         index_sorted += 1
 
     print(a_list)
-b = [100]
+b = [300, 500, 600, 100, 450023, 36, 29, 16, 21, 22, 32, 35, 2, 40, 11, 3, 15, 17, 2, 3]
 insertion_sort(b)
--- Algorithms/Algorithm Tests.py
+++ Algorithms/Algorithm Tests.py
@@ -1,13 +1,25 @@
-def bubble_sort(list):
-    for i in range(len(list)):
-        swapped = False
-        for j in range(0, len(list)-1-i):
-            print(j, i)
-            if list[j] > list[j+1]:
-                list[j],list[j+1] = list[j+1],list[j]
-                swapped = True
-print(bubble_sort([5,4,3,2,1]))
+# def bubble_sort(list):
+#     for i in range(len(list)):
+#         swapped = False
+#         for j in range(0, len(list)-1-i):
+#             print(j, i)
+#             if list[j] > list[j+1]:
+#                 list[j],list[j+1] = list[j+1],list[j]
+#                 swapped = True
+# print(bubble_sort([5,4,3,2,1]))
+#
+# a = [0, 1, 2, 3]
+# a.insert(-1, 5)
+# print(a)
 
-a = [0, 1, 2, 3]
-a.insert(-1, 5)
-print(a)
+def remove_x(s):
+    if 'x' not in s:
+        return s
+    elif 'x' != s[0]:
+        return s[0] + remove_x(s[1:])
+    elif 'x' == s[0]:
+        return remove_x(s[1:])
+    else:
+        return
+
+print(remove_x('xaxaxaxaxa'))
