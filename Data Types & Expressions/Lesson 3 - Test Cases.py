6f95496bfc53e5fb4d99c39d6ceeeb47fd1b6e54
ryanzhao2 <ryan.zhao2@gmail.com>
2022-04-11 23:03:26 -0400

    Ryan commit 2022/4/11

---
+++ Data Types & Expressions/Lesson 3 - Test Cases.py
@@ -0,0 +1,22 @@
+#Q1
+# Generate three (3) test case scenarios for the function count_birthday_duplicate(month, day)
+# Provided a birthday (Month Day   for example July 20)
+#
+# Loop to generate random birthday dates until the provided birthday is generated.
+# Keep track of how many times it takes to generate the duplicate birthday.
+#
+# Return the number of times counted.
+
+#Test Case 1 - Accurate count by generating the random birthday once
+#result = count_birthday_duplicate(September, 27)
+#print(result == 1)
+
+#Test Case 2 - Invalid birthday
+#result = count_birthday_duplicate(January, 31)
+#print(result == infinite)
+
+#Test Case 3 -
+# count = 0
+# while 1 != 0:
+#     count += 1
+#     print(count)
