6bcbf6d9ea8b3f2cea37b97ea1079aa384c8cba6
ryanzhao2 <ryan.zhao2@gmail.com>
2022-04-11 23:02:43 -0400

    Ryan commit 2022/04/11

--- Data Types & Expressions/Lesson 1.py
+++ Data Types & Expressions/Lesson 1.py
@@ -2,3 +2,32 @@
 
 #a) 37
 #b) 1 + 8 + 16 + 32 + 64 + 128 + 32768 = 33017
+
+#Q2
+#1001 1100
+
+#Q3
+#You cannot use 8 bits to represent -400 because the smallest signed number you can get with 8 bits is -128
+
+#Q4
+# def DecimalToBinary(num):
+#         if num >= 1:
+#            DecimalToBinary(num // 2)
+#         print(num % 2)
+#
+# DecimalToBinary(52)
+
+#Q5
+
+def ride_wait_time(p):
+    trains = 3
+    riders_per_train = 24
+    load = 300
+    duration = 205
+    rides_left = (p // (riders_per_train * trains))
+    wait_time_minutes = (rides_left * load + rides_left * duration) / 60
+    # for i in range(rides_left, 0, -1):
+    #     print((i * load + i * duration) / 60)
+    return wait_time_minutes
+
+print(ride_wait_time(200))
--- Data Types & Expressions/Lesson 2.py
+++ Data Types & Expressions/Lesson 2.py
@@ -1,3 +1,10 @@
 #Q1
 
-#a) 1111 . 0001 significand is 0001, exponent is
+#a) NUM IS 15.0625
+# 1111 . 0001 significand is 1110, exponent is 3, sign is 0
+# converts to 0.011 1110
+
+#b) NUM IS -7.6875
+# -0111 1011 significand is 1110, exponent is 2, sign is 1
+# converts to 1.010 1110
+#1. 010 1110
