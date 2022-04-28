8324a4949f93780e852108737b76a7e1b88cc3e9
ryanzhao2 <ryan.zhao2@gmail.com>
2022-04-28 16:47:42 -0400

    Ryan commit 2022/4/28

--- Data Types & Expressions/Lesson 4.py
+++ Data Types & Expressions/Lesson 4.py
@@ -1,18 +1,19 @@
 #Q1
 
-# def do_stuff_with_number(n):
-#     print(n)
-#
-# def main():
-#     the_list = [1, 2, 3, 4, 5]
-#
-#     for i in range(20):
-#         try:
-#             do_stuff_with_number(the_list[i])
-#         except IndexError:
-#             return None
-#
-# main()
+def do_stuff_with_number(n):
+    print(n)
+
+def main():
+    the_list = [1, 2, 3, 4, 5]
+
+    for i in range(20):
+        try:
+            do_stuff_with_number(the_list[i])
+        except IndexError as err:
+            print(err)
+            return None
+
+main()
 
 #Q2
 
