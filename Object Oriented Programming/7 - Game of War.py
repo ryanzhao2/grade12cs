3e91af251e08bb7cfe14988f91eacfddd0860758
ryanzhao2 <ryan.zhao2@gmail.com>
2022-05-10 23:34:10 -0400

    Ryan commit 2022/05/10

--- Object Oriented Programming/7 - Game of War.py
+++ Object Oriented Programming/7 - Game of War.py
@@ -27,6 +27,12 @@ class Card:
     def __eq__(self, other_card):
         return self.value() == other_card.value()
 
+    def __lt__(self, other_card):
+        return self.value() < other_card.value()
+
+    def __gt__(self, other_card):
+        return self.value() > other_card.value()
+
 
 class Deck:
     _numCards = 52
@@ -34,7 +40,6 @@ class Deck:
     _suits = ['hearts', 'spades', 'clubs', 'spades']
 
     def __init__(self):
-
         self._cards = Deck._generate_cards()
 
     @classmethod
@@ -62,53 +67,79 @@ class Deck:
             random.shuffle(self._cards)
 
     def __len__(self):
-        pass
+        return len(self._cards)
 
     def empty(self):
-        pass
+        if len(self._cards) == 0:
+            return True
+        return False
 
     def deal(self):
-        pass
+        return self._cards.pop()
 
+    def deal_pile(self, num):
+        card_list = []
+        for i in range(num):
+            card_list.append(self.deal())
+        return card_list
+
+    def add_lots(self, cards):
+        self._cards.extend(cards)
 
 class Hand(Deck):
     pass
 
 class Player(Deck):
-    def game_of_war():
-        d = Deck()
-        d.shuffle()
-        player1 =
-        player2 =
-        player1.add_lots(d.deal_pile(26))
-        player2.add_lots(d.deal_pile(26))
-        pile = []
-        while not player1.empty() and not player2.empty():
-            c1 = player1.deal()
-            c2 = player2.deal()
-            pile.append(c1)
-            pile.append(c2)
-            if c1 > c2:
-                player1.add_lots(pile)
-                pile = []
+    def __init__(self, name):
+        self._name = name
+        self._cards = Deck._generate_cards()
 
-            elif c2 > c1:
+
+    def __str__(self):
+        return f'{self._name}'
+
+
+def game_of_war():
+    d = Deck()
+    d.shuffle()
+    player1 = Player('1')
+    player2 = Player('2')
+    player1.add_lots(d.deal_pile(26))
+
+    player2.add_lots(d.deal_pile(26))
+    pile = []
+
+    while not player1.empty() and not player2.empty():
+        c1 = player1.deal()
+        c2 = player2.deal()
+        pile.append(c1)
+        pile.append(c2)
+        if c1 > c2:
+            player1.add_lots(pile)
+            pile = []
+
+        elif c2 > c1:
+            player2.add_lots(pile)
+            pile = []
+
+        else:
+            if len(player1) >= 4 and len(player2) >= 4:
+                for i in range(3):
+                    pile.append(player1.deal())
+                    pile.append(player2.deal())
+            elif len(player1) < 4:
                 player2.add_lots(pile)
-                pile = []
 
             else:
-                if len(player1) >= 4 and len(player2) >= 4:
-                    for i in range(3):
-                        pile.append(player1.deal())
-                        pile.append(player2.deal())
-                elif len(player1) < 4:
-                    player2.add_lots(pile)
-
-                else:
-                    player1.add_lots(pile)
+                player1.add_lots(pile)
+    if len(player1._cards) > 0:
+        print(f'winner p1{player1._cards}')
+    else:
+        print(f'winner p2{player2._cards}')
 # Application
 def game():
     input("Press any key to continue")
 
 
-game()
+# game()
+game_of_war()
