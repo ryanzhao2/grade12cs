e1054b335dcca4470652c8eb49e49fdbcbcc0edb
ryanzhao2 <ryan.zhao2@gmail.com>
2022-06-17 14:34:16 -0400

    Ryan commit 2022/06/17

--- Final/Final Coding Project.py
+++ Final/Final Coding Project.py
@@ -1,7 +1,8 @@
 import csv
 
-class Car:
+class Automobile:
     def __init__(self, brand, doors, type, price):
+        self._automobile_dict = {}
         self._brand = brand
         self._doors = doors
         self._type = type
@@ -13,51 +14,59 @@ class Car:
     def doors(self):
         return self._doors
 
-class Convertible(Car):
-    def __init__(self, brand, doors, type, price):
-        super().__init__(brand, doors)
-        self._type = type
+    def price(self):
+        return self._price
+
+    def __lt__(self, other):
+        return self._price < other.price()
+
+    def __gt__(self, other):
+        return self._price > other.price()
+
+    def __eq__(self, other):
+        return self._price == other.price()
 
     def __str__(self):
-        return f'{self._brand} with {self._doors} and is a {self._type}'
+        return f'The {self._brand} car with {self._doors} is a {self._type} and costs {self._price}'
 
     def __repr__(self):
         return f'Car(brand={self._brand}, doors={self._doors}, type={self._type})'
 
+    def add_car(self, type, details):
+        self._automobile_dict[type].append(details)
+
+
+
+class Convertible(Automobile):
+
+    def list_convertible(self):
+        return [self._brand, self._doors, self._doors, self._price]
+
     def resale_value(self):
         return self._price * 0.84
 
-class Sedan(Car):
-    def __init__(self, brand, doors, type, price):
-        super().__init__(brand, doors)
-        self._type = type
+class Sedan(Automobile):
+    sedan_list = []
 
-    def __str__(self):
-        return f'{self._brand},{self._doors}, {self._type}'
 
     def resale_value(self):
         return self._price * 0.76
 
-class Wagon(Car):
-    def __init__(self, brand, doors, type):
-        super().__init__(brand, doors)
-        self._type = type
+class Wagon(Automobile):
+    wagon_list = []
 
     def resale_value(self):
         return
 
-class Hatchback(Car):
-    def __init__(self, brand, doors, type):
-        super().__init__(brand, doors)
-        self._type = type
+class Hatchback(Automobile):
+    hatchback_list = []
+
 
     def resale_value(self):
         return self._price * 0.80
 
 #main
 
-convertables = []
-sedans = []
 def all_automobiles(file):
     with open(file, 'r', encoding='utf-8') as automobile_data:
         dict = {}
@@ -70,16 +79,17 @@ def all_automobiles(file):
             price = line[25]
             if type == 'convertible':
                 a_convertible = Convertible(brand, doors, type)
-                convertables.append(a_convertible)
-                print(a_convertible)
             elif type == 'sedan':
                 a_sedan = Sedan(brand, doors, type)
-                sedans.append(a_convertible)
-                print(a_sedan)
+            elif type == 'hatchback':
+                a_hatchback = Sedan(brand, doors, type)
             all = [brand, doors, type, price]
             if type not in dict:
                 dict[type] = []
             dict[type].append(all)
         #print(convertables)
 
-all_automobiles('Automobile_data.csv')
+def main():
+    all_automobiles('Automobile_data.csv')
+
+main()
--- Object Oriented Programming/5 - Inheritance.py
+++ Object Oriented Programming/5 - Inheritance.py
@@ -1,162 +1,162 @@
-#
-# import random
-#
-# class PostSecondary:
-#
-#     _a = '123'
-#
-#     def __init__(self, name):
-#         self._name = name
-#         self._students = []
-#         self._applicants = []
-#
-#     def name(self):
-#         return self._name
-#
-#     def type(self):
-#         return self._type
-#
-#     def __str__(self):
-#         return (f'{self._name} with {len(self._students)} students enrolled')
-#
-#     def __repr__(self):
-#         return (f'class PostSecondary: name={self._name}, students={self._students}, applicants={self._applicants}')
-#
-#     def print_applicants(self):
-#         s = ""
-#         for a in self._applicants:
-#             s += a.__str__() + "\n"
-#         return s
-#
-#     def get_applicants(self):
-#         return self._applicants
-#
-#     def print_students(self):
-#         s = ""
-#         for a in self._students:
-#             s += a.__str__() + "\n"
-#         return s
-#
-#     def get_students(self):
-#         return self._students
-#
-#     def add_student(self, student):
-#         if student not in self._students:
-#             self._students.append(student)
-#
-#     def add_applicant(self, applicant):
-#         if applicant not in self._applicants:
-#             self._applicants.append(applicant)
-#
-#     def reject_application(self, applicant):
-#
-#         if applicant in self._applicants:
-#             self._applicants.remove(applicant)
-#
-# class Student:
-#
-#     def __init__(self, name, courses, average):
-#         self._name = name
-#         self._courses = courses
-#         self._average = average
-#
-#     def name(self):
-#         return self._name
-#
-#     def courses(self):
-#         return self._courses
-#
-#     def average(self):
-#         return self._average
-#
-#     def __str__(self):
-#         return (f'{self._name:<10}, average={self._average:<4}, courses={self._courses}')
-#
-#     def __repr__(self):
-#         return (f'class Student name={self._name}, avg={self._average}, courses={self._courses}')
-#
-#
-#
-# class College(PostSecondary):
-#     def accept_applications(self):
-#         for applicant in self._applicants:
-#             if 'ENG4U' or 'ENG4C' and 'MHF4U' or 'MCT4M' in applicant.courses():
-#                 if applicant.average() >= 80:
-#                     self._students.append(applicant)
-#     def print(self):
-#         print(College._a)
-#
-#
-# class University(PostSecondary):
-#
-#     def accept_applications(self):
-#         for applicant in self._applicants:
-#             if 'ENG4U' and 'MHF4U' and 'ICS4U' in applicant.courses() and applicant.average() >= 85:
-#                 self._students.append(applicant)
-#
-#
-# def generate_random_students():
-#     names = ['Nabeela Gentry', 'Ishika Lutz', 'Sullivan Johnston', 'Rhiannan Lambert', 'Elijah Odling', 'Hanna Salazar', 'Kimora Nicholson', 'Siobhan Samuels', 'Bianka Steadman', 'Sianna Franco', 'Sierra Greaves', 'Ailish Pemberton', 'Katie Luna', 'Jak Garcia', 'Summer-Louise Hughes', 'Riley Hancock', 'Garfield Moss', 'Karishma Hodgson', 'Darren Castaneda', 'Tasneem Dalton', 'Leanne Turner', 'Priyanka Read', 'Mahnoor Paine', 'Savannah Lamb', 'Waseem Wynn', 'Ruben Hartley', 'Izzie Richardson', 'Cem Scott', 'Eren Hood', 'Marc Lane']
-#     courses = ["MHF4U", "ENG4U", "ICS4U", "MCT4M", "ENG4C", "MDM4U", "EWC4U"]
-#
-#     all_students = []
-#
-#     for n in names:
-#
-#         # pick 4 unique courses
-#         courseList = []
-#         while len(courseList) < 4:
-#             c = random.choice(courses)
-#             if c not in courseList:
-#                 courseList.append(c)
-#
-#         avg = random.choice([70, 75, 80, 85, 90, 95])
-#
-#         s = Student(n, courseList, avg)
-#         all_students.append(s)
-#
-#     return all_students
-#
-#
-# def load_applicants(institution, list_students):
-#
-#     for current_student in list_students:
-#         institution.add_applicant(current_student)
-#
-#
-# def main():
-#
-#     # imagine that we created even more institutions
-#     guelph_university = University("Guelph")
-#     mohawk_college = College("Mohawk")
-#     all_postsecondary = [guelph_university, mohawk_college]
-#     # generate random students
-#     all_students = generate_random_students()
-#
-#     # add applications to institution database
-#     for institution in all_postsecondary:
-#         load_applicants(institution, all_students)
-#
-#
-#     for institution in all_postsecondary:
-#         print(f'\n\n{institution.name()}')
-#         print("Applicants are")
-#         print("- " *30)
-#         print(institution.print_applicants())
-#
-#     # perform the acceptance check for applicants
-#     for institution in all_postsecondary:
-#         institution.accept_applications()
-#
-#
-#     for institution in all_postsecondary:
-#         print(f'\n\n{institution.name()}')
-#         print("Acceptances go out to")
-#         print("- " *30)
-#         print(institution.print_students())
-#
-#
-# main()
+
+import random
+
+class PostSecondary:
+
+    _a = '123'
+
+    def __init__(self, name):
+        self._name = name
+        self._students = []
+        self._applicants = []
+
+    def name(self):
+        return self._name
+
+    def type(self):
+        return self._type
+
+    def __str__(self):
+        return (f'{self._name} with {len(self._students)} students enrolled')
+
+    def __repr__(self):
+        return (f'class PostSecondary: name={self._name}, students={self._students}, applicants={self._applicants}')
+
+    def print_applicants(self):
+        s = ""
+        for a in self._applicants:
+            s += a.__str__() + "\n"
+        return s
+
+    def get_applicants(self):
+        return self._applicants
+
+    def print_students(self):
+        s = ""
+        for a in self._students:
+            s += a.__str__() + "\n"
+        return s
+
+    def get_students(self):
+        return self._students
+
+    def add_student(self, student):
+        if student not in self._students:
+            self._students.append(student)
+
+    def add_applicant(self, applicant):
+        if applicant not in self._applicants:
+            self._applicants.append(applicant)
+
+    def reject_application(self, applicant):
+
+        if applicant in self._applicants:
+            self._applicants.remove(applicant)
+
+class Student:
+
+    def __init__(self, name, courses, average):
+        self._name = name
+        self._courses = courses
+        self._average = average
+
+    def name(self):
+        return self._name
+
+    def courses(self):
+        return self._courses
+
+    def average(self):
+        return self._average
+
+    def __str__(self):
+        return (f'{self._name:<10}, average={self._average:<4}, courses={self._courses}')
+
+    def __repr__(self):
+        return (f'class Student name={self._name}, avg={self._average}, courses={self._courses}')
+
+
+
+class College(PostSecondary):
+    def accept_applications(self):
+        for applicant in self._applicants:
+            if 'ENG4U' or 'ENG4C' and 'MHF4U' or 'MCT4M' in applicant.courses():
+                if applicant.average() >= 80:
+                    self._students.append(applicant)
+    def print(self):
+        print(College._a)
+
+
+class University(PostSecondary):
+
+    def accept_applications(self):
+        for applicant in self._applicants:
+            if 'ENG4U' and 'MHF4U' and 'ICS4U' in applicant.courses() and applicant.average() >= 85:
+                self._students.append(applicant)
+
+
+def generate_random_students():
+    names = ['Nabeela Gentry', 'Ishika Lutz', 'Sullivan Johnston', 'Rhiannan Lambert', 'Elijah Odling', 'Hanna Salazar', 'Kimora Nicholson', 'Siobhan Samuels', 'Bianka Steadman', 'Sianna Franco', 'Sierra Greaves', 'Ailish Pemberton', 'Katie Luna', 'Jak Garcia', 'Summer-Louise Hughes', 'Riley Hancock', 'Garfield Moss', 'Karishma Hodgson', 'Darren Castaneda', 'Tasneem Dalton', 'Leanne Turner', 'Priyanka Read', 'Mahnoor Paine', 'Savannah Lamb', 'Waseem Wynn', 'Ruben Hartley', 'Izzie Richardson', 'Cem Scott', 'Eren Hood', 'Marc Lane']
+    courses = ["MHF4U", "ENG4U", "ICS4U", "MCT4M", "ENG4C", "MDM4U", "EWC4U"]
+
+    all_students = []
+
+    for n in names:
+
+        # pick 4 unique courses
+        courseList = []
+        while len(courseList) < 4:
+            c = random.choice(courses)
+            if c not in courseList:
+                courseList.append(c)
+
+        avg = random.choice([70, 75, 80, 85, 90, 95])
+
+        s = Student(n, courseList, avg)
+        all_students.append(s)
+
+    return all_students
+
+
+def load_applicants(institution, list_students):
+
+    for current_student in list_students:
+        institution.add_applicant(current_student)
+
+
+def main():
+
+    # imagine that we created even more institutions
+    guelph_university = University("Guelph")
+    mohawk_college = College("Mohawk")
+    all_postsecondary = [guelph_university, mohawk_college]
+    # generate random students
+    all_students = generate_random_students()
+
+    # add applications to institution database
+    for institution in all_postsecondary:
+        load_applicants(institution, all_students)
+
+
+    for institution in all_postsecondary:
+        print(f'\n\n{institution.name()}')
+        print("Applicants are")
+        print("- " *30)
+        print(institution.print_applicants())
+
+    # perform the acceptance check for applicants
+    for institution in all_postsecondary:
+        institution.accept_applications()
+
+
+    for institution in all_postsecondary:
+        print(f'\n\n{institution.name()}')
+        print("Acceptances go out to")
+        print("- " *30)
+        print(institution.print_students())
+
+
+main()
 
 
 
